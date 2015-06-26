b9ee318d16db4749b0e6fec2e2d51c10f8751bd4
edcilo <edcilop@gmail.com>
2015-06-26 18:55:25 -0500

    borrando codigo blade comentado que causa errores en windows

--- app/views/productDescription/partials/formCreate.blade.php
+++ app/views/productDescription/partials/formCreate.blade.php
@@ -68,28 +68,6 @@
             {{ $errors->first('stock_max', '<span>:message</span>') }}
         </div>
     </div>
-{{--
-    <div class="flo col30 row center text-center">
-        {{ Form::label('box', '¿El producto se compra por caja?') }} <br/>
-        <div class="flo col50">
-            {{ Form::radio('box', 1, !is_null($desc) ? $desc->box == 1 : false) }} Si
-        </div>
-        <div class="flo col50">
-            {{ Form::radio('box', 0, !is_null($desc) ? $desc->box == 0 : true) }} No
-        </div>
-        <div class="flo col100 message-error">
-            {{ $errors->first('box', '<span>:message</span>') }}
-        </div>
-    </div>
-
-    <div class="flo col20 row right">
-        {{ Form::label('pieces', 'Número de piezas por caja: ') }}
-        {{ Form::text('pieces', !is_null($desc) ? $desc->pieces : 0, ['class'=>'text-right xs-input', 'title'=>'Este campo es obligatorio y debe tener un valor entero positivo.', 'data-required'=>'required', 'data-integer' => 'integer']) }}
-        <div class="message-error">
-            {{ $errors->first('pieces', '<span>:message</span>') }}
-        </div>
-    </div>
-    --}}
 
     <div class="flo col20 row center">
         {{ Form::label('provider', 'Proveedor: ') }}

93f2cff15d7f0cfedc2a87ac4aeb11e3c9080b8b
edcilo <edcilop@gmail.com>
2015-07-07 18:54:07 -0500

    corrigiendo ventanas d confirmacion en marcas y categorias

--- app/views/category/partials/form_destroy.blade.php
+++ app/views/category/partials/form_destroy.blade.php
@@ -3,14 +3,14 @@
 
     {{ Form::open(['route'=>['category.destroy', $category->id], 'method'=>'delete', 'class'=>'form']) }}
     <div class="row text-right">
-        <button class="btn-red form_confirm">
+        <button class="btn-red form_confirm" data-confirm="destroy_confirm">
             <i class="fa fa-times"></i> Eliminar categoría
         </button>
     </div>
     {{ Form::close() }}
 
 
-    <div class="confirm-dialog hide" title="Eliminar categoría" id="formConfirm" data-width="400">
+    <div class="confirm-dialog hide" title="Eliminar categoría" id="destroy_confirm" data-width="400">
         <div class="mesasge text-center">
             <p>¿Estas seguro de querer eliminar la categoría <strong>{{ $category->name }}</strong>?</p>
         </div>
--- app/views/mark/partials/form_destroy.blade.php
+++ app/views/mark/partials/form_destroy.blade.php
@@ -3,14 +3,14 @@
 
     {{ Form::open(['route'=>['mark.destroy', $mark->id], 'method'=>'delete', 'class'=>'form']) }}
     <div class="row text-right">
-        <button class="btn-red form_confirm">
+        <button class="btn-red form_confirm" data-confirm="destroy_confirm">
             <i class="fa fa-times"></i> Eliminar marca
         </button>
     </div>
     {{ Form::close() }}
 
 
-    <div class="confirm-dialog hide" title="Eliminar marca" id="formConfirm" data-width="400">
+    <div class="confirm-dialog hide" title="Eliminar marca" id="destroy_confirm" data-width="400">
         <div class="mesasge text-center">
             <p>¿Estas seguro de querer eliminar la marca <strong>{{ $mark->name }}</strong>?</p>
         </div>

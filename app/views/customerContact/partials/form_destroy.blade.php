3a3f9fb448d19ef4a09bd9a0af50d729336f90e7
edcilo <edcilop@gmail.com>
2015-07-07 18:02:09 -0500

    ventanas de confirmacion de contactos del cliente

--- app/views/customerContact/partials/form_destroy.blade.php
+++ app/views/customerContact/partials/form_destroy.blade.php
@@ -1,7 +1,15 @@
 @if( p(65) )
 
     {{ Form::open(['route'=>['customer.contact.destroy',  $contact->id], 'method'=>'delete', 'class'=>'inline']) }}
-    <button type="submit" class="btn-red"><i class="fa fa-times"></i></button>
+        <button type="submit" class="btn-red form_confirm" data-confirm="destroy_contact_confirm">
+            <i class="fa fa-times"></i>
+        </button>
     {{ Form::close() }}
 
-    @endif
+    <div class="confirm-dialog hide" title="Eliminar contacto" id="destroy_contact_confirm" data-width="400">
+        <div class="mesasge text-center">
+            <p>¿Estas seguro de querer eliminar al contacto <strong>{{ $contact->dataContact->name }}</strong>?</p>
+        </div>
+    </div>
+
+@endif

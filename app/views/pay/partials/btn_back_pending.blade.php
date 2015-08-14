3f7488efeb0117ed103ad30df599b917653d49a3
edcilo <edcilop@gmail.com>
2015-08-14 12:08:57 -0500

    cambiando el tamaño del boton de volver a la caja de pagos

--- app/views/pay/partials/btn_back_pending.blade.php
+++ app/views/pay/partials/btn_back_pending.blade.php
@@ -1,5 +1,5 @@
 <div class="flo col50">
-    <a href="{{ route('pay.pending') }}" class="btn-red">
+    <a href="{{ route('pay.pending') }}" class="btn-metro-red">
         <i class="fa fa-arrow-left"></i>
         Volver a la caja
     </a>
